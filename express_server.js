7c2e7bed0b53a2fb6c5c632bf935cb04db895ac6
bbashcode <binbash.code@gmail.com>
2022-01-25 05:37:50 +0000

    URL Shortening: (Part 1) completed. However, urls/new endpoint is not working as expected

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; //default port
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,9 +15,15 @@ app.get("/", (req, res) =>{
   res.send("Hello");
 });
 
+//setting up GET route to render the urls_new.ejs template
+app.get("urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 
 //adding a new route for /urls
 app.get("/urls", (req, res) => {
@@ -23,12 +31,36 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+
 //setting up another route and adding template
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+function generateRandomString() {
+  let length = 6;
+  let shortURL = "";
+  
+  const alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for(let i = 0; i <= length; i++){
+    let randomIndex = Math.floor(Math.random() * alphaNumeric.length);
+    shortURL += alphaNumeric.substring(randomIndex, randomIndex + 1)
+  }
+
+  return shortURL;
+}
